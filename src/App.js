e16db7b73211e433c63a66e587779ee02032b42e
Tamer Osman <TamerARagab.Osman@btc-ag.com>
2018-05-09 18:55:42 +0200

    2nd lesson

--- src/App.js
+++ src/App.js
@@ -1,10 +1,17 @@
 import React from "react";
-class App extends React.Component{
-    render(){
+import PropTypes from 'prop-types';
+class App extends React.Component {
+    render() {
+        let txt = this.props.txt
         return (<div>
-                    <h1>Hello World</h1>
-                    <b>Bold</b>
-                </div>)
+            <h1>{txt}</h1>
+            <b>Bold</b>
+        </div>
+        )
     }
+}
+App.propTypes = {
+    txt: PropTypes.string,
+    cat: PropTypes.number
 }
 export default App
