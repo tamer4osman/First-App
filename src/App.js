69e136a0c76793297478abe5eb7b24adf0cb19d7
Tamer Osman <TamerARagab.Osman@btc-ag.com>
2018-05-09 16:20:28 +0200

    1st lesson

--- src/App.js
+++ src/App.js
@@ -1,3 +1,10 @@
 import React from "react";
-const App = () => <h1>Hello</h1>
+class App extends React.Component{
+    render(){
+        return (<div>
+                    <h1>Hello World</h1>
+                    <b>Bold</b>
+                </div>)
+    }
+}
 export default App
